eb87c7b3707ea7ed299f167170d4e32d89ef2024
David Turner <david@davidturner.name>
2014-12-31 03:36:49 +0000

    Should now properly install everything. I think.

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 git clone https://github.com/davidturner/dotfiles.git "$HOME/.dotfiles"
-# chmod +x "$HOME/.dotfiles/.osx"
-# source "$HOME/.dotfiles/.osx"
+chmod +x "$HOME/.dotfiles/.osx"
+source "$HOME/.dotfiles/.osx"
